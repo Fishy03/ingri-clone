926ba7cd3eaa805b67627383c5b9f2c865b4e711
Yash Raj Nagrath <yashrajnagrath07@gmail.com>
2026-07-13 05:12:17 +0530

    testing recipe route

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,19 +1,33 @@
-const Recipe = require('../models/Recipe');
+// const Recipe = require('../models/Recipe');
+
+// const getAllRecipes = async (req, res) => {
+//   try {
+//     const recipes = await Recipe.find();
+//     res.status(200).json({
+//       success: true,
+//       data: recipes
+//     });
+//   } catch (error) {
+//     console.error('Fetch recipes error:', error);
+//     res.status(500).json({
+//       success: false,
+//       message: 'Server error. Please try again later.'
+//     });
+//   }
+// };
+
+// module.exports = { getAllRecipes };
 
 const getAllRecipes = async (req, res) => {
-  try {
-    const recipes = await Recipe.find();
-    res.status(200).json({ 
-      success: true, 
-      data: recipes 
-    });
-  } catch (error) {
-    console.error('Fetch recipes error:', error);
-    res.status(500).json({ 
-      success: false, 
-      message: 'Server error. Please try again later.' 
-    });
-  }
+  res.status(200).json({
+    success: true,
+    data: [
+      {
+        title: "Test Recipe",
+        description: "Render is working",
+      },
+    ],
+  });
 };
 
 module.exports = { getAllRecipes };
